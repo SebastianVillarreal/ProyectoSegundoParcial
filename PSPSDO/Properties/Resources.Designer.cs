e1a6ddb4cb48ac6b832163628a8e627520d7b11a
GustavoAngel2 <124698903+GustavoAngel2@users.noreply.github.com>
2023-03-28 07:42:12 -0600

    solo jonathan merge
    para probar imagenes

--- PSPSDO/Properties/Resources.Designer.cs
+++ PSPSDO/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace PSPSDO.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap New_Piskel {
+            get {
+                object obj = ResourceManager.GetObject("New Piskel", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
