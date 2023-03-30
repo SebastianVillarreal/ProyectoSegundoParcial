39f17ff07e4535756949711099acc1a2a353a58e
JorgeRub-Rey <12779@utl.edu.mx>
2023-03-29 19:31:24 -0600

    ,m,m

--- PSPSDO/Forms/frmReportesAlumnos.Designer.cs
+++ PSPSDO/Forms/frmReportesAlumnos.Designer.cs
@@ -47,41 +47,41 @@
             this.gbRA.Controls.Add(this.btnCrearRA);
             this.gbRA.Controls.Add(this.lblRANombre);
             this.gbRA.Controls.Add(this.txtRANombre);
-            this.gbRA.Location = new System.Drawing.Point(19, 41);
-            this.gbRA.Margin = new System.Windows.Forms.Padding(2);
+            this.gbRA.Location = new System.Drawing.Point(25, 50);
+            this.gbRA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gbRA.Name = "gbRA";
-            this.gbRA.Padding = new System.Windows.Forms.Padding(2);
-            this.gbRA.Size = new System.Drawing.Size(1042, 230);
+            this.gbRA.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gbRA.Size = new System.Drawing.Size(1389, 283);
             this.gbRA.TabIndex = 0;
             this.gbRA.TabStop = false;
             // 
             // btnCrearRA
             // 
             this.btnCrearRA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnCrearRA.Location = new System.Drawing.Point(720, 88);
-            this.btnCrearRA.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCrearRA.Location = new System.Drawing.Point(960, 108);
+            this.btnCrearRA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCrearRA.Name = "btnCrearRA";
-            this.btnCrearRA.Size = new System.Drawing.Size(239, 42);
+            this.btnCrearRA.Size = new System.Drawing.Size(319, 52);
             this.btnCrearRA.TabIndex = 2;
             this.btnCrearRA.Text = "Genera Reportes";
             this.btnCrearRA.UseVisualStyleBackColor = true;
+            this.btnCrearRA.Click += new System.EventHandler(this.btnCrearRA_Click);
             // 
             // lblRANombre
             // 
             this.lblRANombre.AutoSize = true;
-            this.lblRANombre.Location = new System.Drawing.Point(13, 91);
-            this.lblRANombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRANombre.Location = new System.Drawing.Point(17, 112);
             this.lblRANombre.Name = "lblRANombre";
-            this.lblRANombre.Size = new System.Drawing.Size(114, 36);
+            this.lblRANombre.Size = new System.Drawing.Size(145, 45);
             this.lblRANombre.TabIndex = 1;
             this.lblRANombre.Text = "Nombre:";
             // 
             // txtRANombre
             // 
-            this.txtRANombre.Location = new System.Drawing.Point(131, 88);
-            this.txtRANombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtRANombre.Location = new System.Drawing.Point(175, 108);
+            this.txtRANombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtRANombre.Name = "txtRANombre";
-            this.txtRANombre.Size = new System.Drawing.Size(172, 43);
+            this.txtRANombre.Size = new System.Drawing.Size(228, 52);
             this.txtRANombre.TabIndex = 0;
             this.txtRANombre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -90,12 +90,12 @@
             this.dgvReportesAlumno.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dgvReportesAlumno.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dgvReportesAlumno.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvReportesAlumno.Location = new System.Drawing.Point(19, 291);
-            this.dgvReportesAlumno.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvReportesAlumno.Location = new System.Drawing.Point(25, 358);
+            this.dgvReportesAlumno.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvReportesAlumno.Name = "dgvReportesAlumno";
             this.dgvReportesAlumno.RowHeadersWidth = 62;
             this.dgvReportesAlumno.RowTemplate.Height = 28;
-            this.dgvReportesAlumno.Size = new System.Drawing.Size(1042, 261);
+            this.dgvReportesAlumno.Size = new System.Drawing.Size(1389, 321);
             this.dgvReportesAlumno.TabIndex = 1;
             // 
             // groupBox1
@@ -106,9 +106,11 @@
             this.groupBox1.Controls.Add(this.gbRA);
             this.groupBox1.Controls.Add(this.dgvReportesAlumno);
             this.groupBox1.Font = new System.Drawing.Font("Impact", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 5);
+            this.groupBox1.Location = new System.Drawing.Point(16, 6);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1706, 974);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(2275, 1199);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Reportes Alumnos";
@@ -118,19 +120,20 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = global::PSPSDO.Properties.Resources.ut_menu2;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(1124, 21);
+            this.pictureBox1.Location = new System.Drawing.Point(1499, 26);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(137, 105);
+            this.pictureBox1.Size = new System.Drawing.Size(183, 129);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // frmReportesAlumnos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1285, 749);
+            this.ClientSize = new System.Drawing.Size(1713, 922);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmReportesAlumnos";
             this.Text = "frmReportesAlumnos";
             this.Load += new System.EventHandler(this.frmReportesAlumnos_Load);
